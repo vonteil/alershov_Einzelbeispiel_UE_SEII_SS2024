c7b2311b54e98e447630b9d146ab8f3a3cb45d17
[Aleksandr <[alershov@edu.aau,at]>
2024-03-09 22:58:55 +0100

    Internet permission added in Manifest.xml

--- app/src/main/java/com/example/einzelbeispiel_21/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_21/MainActivity.java
@@ -1,37 +1,87 @@
 package com.example.einzelbeispiel_21;
 
+//import static com.google.android.material.button.MaterialButtonToggleGroup.CornerData.start;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String hostName = "se2-submission.aau.at";
+    private final int portNumber = 20080;
+    private EditText nummer;
+    private TextView serverReply;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        /*ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello METANIT.COM!");
-        textView.setTextSize(28);
-        setContentView(textView);*/
+        setContentView(R.layout.new_layout);
+
+        Button button = findViewById(R.id.button);
+        button.setEnabled(false);
+        nummer = findViewById(R.id.nummer);
 
 
-        setContentView(R.layout.second_layout);
-        //TextView textView = findViewById(R.id.header);
-        //textView.setText("Hello from AAU");
+        button.setOnClickListener (new View.OnClickListener() {
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
+            @Override
+            public void onClick (View v){
+                onButtonClick();
+            }
+        });
+
+        serverReply = findViewById(R.id.server_reply);
+    }
+
+    protected void onButtonClick (){
+
+        String numberInString = nummer.getText().toString();
+
+        button.setEnabled(true);
+            new Thread (new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Socket newSocket = new Socket(hostName, portNumber);
+
+                        PrintWriter out = new PrintWriter(newSocket.getOutputStream(),true);
+                        out.println(numberInString);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
+                        //BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                        String server_reply = in.readLine();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                serverReply.setText(server_reply);
+                            }
+                        });
+
+                        out.close();
+                        in.close();
+                        newSocket.close();
+
+                    } catch (Exception e) {
+                        e.getMessage();
+                    }
+                }
+            }).start();
     }
 }
